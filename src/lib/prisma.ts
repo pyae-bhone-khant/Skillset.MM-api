fb7d990c58beaf01de33c2158f6733cfd19f3ca7
pyae-bhone-khant <pyaebhonen4@gmail.com>
2026-07-02 23:31:30 +0630

    chore: remove neon serverless adapter and use native prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,10 @@
-import 'dotenv/config'
-import { PrismaClient } from '../../generated/prisma/client.js'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { config } from 'dotenv'
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client.js";
 
-config()
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
-})
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-export const prisma = new PrismaClient({ adapter })
+export { prisma };
